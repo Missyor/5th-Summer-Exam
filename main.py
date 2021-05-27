2a906aec93e44b56dab9fd2c300c43c5b328bf80
Aoife1 <>
2021-05-27 19:20:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+###################
+##Q9
+##Name:
+
+pin = "1579"
+
+userTry = input("Enter pin: ")
+
+if userTry == pin:
+  print("Welcome!")
+
+##Q10
+
+##Fix the code below so that it runs
+
+## a.
+
+guess = int(input("Enter number: "))
+
+if guess > 1
+  print("Yes")
+
+## b.
+
+listName = ["Ann", "Barry", "James"]
+listName.append("Steven")
+print(listname)
+
+## Q11
+
+import random
+
+numbers = (1, 2, 3)
+
+choice = random.choice(numbers)
+
+print(choice)
+
+## Q12
